b999e5f56e0082906925f74b19be44f5509f3342
Suyeon <alveloper@gmail.com>
2021-08-19 15:32:44 +0900

    6. Updating our state with HookState

--- src/FirstPerson.jsx
+++ src/FirstPerson.jsx
@@ -8,6 +8,19 @@ const FirstPerson = () => {
   // which is the state we want to access,
   // and we want the "store" we just created.
   const messageList = useState(store);
+
+  // (5) handleSendMessage function: update the state
+  const handleSendMessage = newMessage => {
+    // inside this .merge method we want to pass on the new value of our states as its argument.
+    // so it's going to take the value and append it to what there was before.
+    // you don't need to use spread operator or something.
+    messageList.merge([{
+      text: newMessage,
+      person: 'secondary' // this is going to be show the message like on the opposite.
+    }])
+    // if you want to replace the old state, then use .set method.
+  }
+
   return (
     <ChatBox
       settings={{
@@ -21,6 +34,13 @@ const FirstPerson = () => {
       // we're going to add it to the message list state and that's going to display inside the chat box
       messageList={messageList.get()} // to access the data from our state, we have to use the get method.
       // => now the browser shows the initial state 'hey' as a message in chatbox of first person.
+
+      // (5) make handleSendMessage method and bind to onSendMessage.
+      // this onSendMessage prop expects a function that is going to tell the chatbox component
+      // what to do whenever a message is sent.
+      // so whenever the user clicks the send button, 
+      // the chatbox component is going to run the onSendMessage function.
+      onSendMessage={handleSendMessage}
     />
   );
 };
